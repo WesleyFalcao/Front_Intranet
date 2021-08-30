ff71d2e6786a60a1c961171016d67d631cb164b4
Wesley <wesleyunimed@outlook.com>
2021-08-30 12:36:34 -0300

    segundo-commit

--- src/app/page/documentos/documentos.component.ts
+++ src/app/page/documentos/documentos.component.ts
@@ -9,10 +9,27 @@ export class DocumentosComponent implements OnInit {
 
   ArrayLista = [
 
-    {
-      
+    { nome: "Documentos do SGQ", }, 
+    { nome: "Linhas de Cuidados", }, 
+    { nome: "Mapa de Processo HU", }, 
+    { nome: "Documentos da Acreditação", }, 
+    { nome: "Protocolos Médicos", }, 
+    { nome: "Normas de Especialidade", }, 
+    { nome: "Segurança do Paciente", }, 
+ 
+  ]
 
-    }
+  ArrayDocumentos = [
+
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Gerenciamento de Demandas de TI", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "IT-NF-001" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Sistemática de Backup", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "PR-NF-004" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Aquisição de Equipamentos na Modalidade Leasing Operacional - Tecnologia de Informação", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "IT-NF-004" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Tabela de Registro: Tecnologia de Informação", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "OD-NF-001" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Aquisição de Equipamentos em Regime de Leasing Operacional", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "PR-NF-005" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Plano de Continuidade de Negócio", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "IT-NF-005" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Levantamento de Requisitos (Requisição de Mudanças)", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "FM-INF-002" },
+    { nm_Processo: " Tecnologia da Informação ", nm_Documento: "Política de Segurança de Informação", nr_Revisao: 0, dt_Revisao: "2021-08-30", nm_Codigo: "POL-DIR-007" },
+
   ]
 
   constructor() { }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,34 @@
 module.exports = {
-    prefix: '',
-    purge: {
-      content: [
-        './src/**/*.{html,ts}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {
-        backgroundImage: theme => ({
-          'imagem-fundo': "url('assets/imagens/background.svg')"
-        })
+  prefix: '',
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        unimed: {
+          green: '#00995D',
+          // ...
+          pink: '#ED1651',
+
+          purpe: '#A3238E',
+
+        },
       },
+
+      backgroundImage: theme => ({
+        'imagem-fundo': "url('assets/imagens/background.svg')"
+
+      })
     },
-    variants: {
-      extend: {
-        transitionProperty : ['hover' , 'focus'],
-      },
+  },
+  variants: {
+    extend: {
+      transitionProperty: ['hover', 'focus'],
     },
-    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
